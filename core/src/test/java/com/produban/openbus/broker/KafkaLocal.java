016e7e5c463b62364223603b6e873b30c43400c0
Javier Alba <me@fjavieralba.com>
2014-01-20 16:22:53 +0100

    Local Kafka instance now shutdowns also Zookeeper instance.

--- core/src/test/java/com/produban/openbus/broker/KafkaLocal.java
+++ core/src/test/java/com/produban/openbus/broker/KafkaLocal.java
@@ -18,6 +18,8 @@ package com.produban.openbus.broker;
 
 import java.io.IOException;
 import java.util.Properties;
+
+import com.produban.openbus.util.ZookeeperLocal;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServerStartable;
 
@@ -25,14 +27,21 @@ import kafka.server.KafkaServerStartable;
 public class KafkaLocal {
 
 	public KafkaServerStartable kafka;
-	public ZooKeeperLocal zookeeper;
+	public ZookeeperLocal zookeeper;
 	
-	public KafkaLocal(Properties kafkaProperties, Properties zkProperties) throws IOException, InterruptedException{
+	public KafkaLocal(Properties kafkaProperties) throws IOException, InterruptedException{
 		KafkaConfig kafkaConfig = new KafkaConfig(kafkaProperties);
 		
 		//start local zookeeper
 		System.out.println("starting local zookeeper...");
-		zookeeper = new ZooKeeperLocal(zkProperties);
+		zookeeper = new ZookeeperLocal();
+        try {
+            zookeeper.startzkServer(2181);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error starting local zookeeper server", e);
+        }
+
 		System.out.println("done");
 		
 		//start local kafka broker
@@ -40,14 +49,21 @@ public class KafkaLocal {
 		System.out.println("starting local kafka broker...");
 		kafka.startup();
 		System.out.println("done");
+
+
 	}
 	
 	
-	public void stop(){
+	public void stop() throws IOException {
 		//stop kafka broker
 		System.out.println("stopping kafka...");
 		kafka.shutdown();
 		System.out.println("done");
+
+        //stop zookeeper local server:
+        zookeeper.stopZkServer();
 	}
-	
+
+
+
 }
