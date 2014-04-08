ca96bb1cd1ecfc18780c7d12f75ff32be8733024
Javier Alba <me@fjavieralba.com>
2014-04-08 11:34:33 +0200

    fixed word-list filter

--- core/src/main/java/com/produban/openbus/analysis/TweetFilter.java
+++ core/src/main/java/com/produban/openbus/analysis/TweetFilter.java
@@ -1,7 +1,11 @@
 package com.produban.openbus.analysis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import storm.trident.operation.BaseFilter;
 import storm.trident.tuple.TridentTuple;
+
+import java.util.List;
 
 import static com.produban.openbus.util.Common.join;
 
@@ -11,15 +15,35 @@ import static com.produban.openbus.util.Common.join;
 public class TweetFilter extends BaseFilter {
 
     String regex;
+    private static final Logger logger = LoggerFactory.getLogger(TweetFilter.class);
 
-    public TweetFilter(String[] keywords) {
-        this.regex = "\\W("+join(keywords, "|") + ")\\W";
+    public TweetFilter(List<String> keywords) {
+        //lowercase text and words
+        logger.info("tweet filter keywords:");
+        for (int i = 0; i < keywords.size(); i++) {
+            keywords.set(i, keywords.get(i).toLowerCase());
+            System.out.println(keywords.get(i));
+        }
+
+        this.regex = ".*\\W("+join(keywords, "|") + ")\\W.*";
     }
 
     @Override
     //filter every tweet not containing at least one keyword
     public boolean isKeep(TridentTuple objects) {
-        String text = objects.getString(0);
-        return text.matches(this.regex) ? true : false;
+        String text = objects.getString(0).toLowerCase();
+        if (text == null){
+            logger.info("no text could be retrieved from tuple");
+            return false;
+        }
+
+        if (text.matches(this.regex)) {
+            logger.info("matches!");
+            return true;
+        }
+        else {
+            logger.info("filtered!");
+            return false;
+        }
     }
 }
