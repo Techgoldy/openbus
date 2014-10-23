59be805fe2c38de0273c2aaa7c8963850274de06
mmesa <mmesa@indra.es>
2014-10-23 11:56:40 +0200

    Old folder deleted

--- pocs/hivehbaseconnector/src/test/resources/putLogInHdfs.sh
+++
@@ -1,2 +0,0 @@
-hadoop fs -mkdir /user/hive/warehouse/metricas/
-hadoop fs -put LogExample.log /user/hive/warehouse/metricas/
--- pocs/stormPOC/run.sh
+++
@@ -1,1 +0,0 @@
-mvn clean package exec:java -Dexec.mainClass="pocs.SiddhiTopology" -Dexec.args="p.txt o2.txt"
